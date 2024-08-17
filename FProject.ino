25b5d4041fecfb140172150f333d3459816818f0
AvshalomSarusi <131193715+AvshalomSarusi@users.noreply.github.com>
2024-08-17 21:23:28 +0300

    Update FProject.ino
    יצירת משתנים שבסופו של דבר יעזרו לי בחישוב הלחיצה הקצרה ביותר

--- FProject.ino
+++ FProject.ino
@@ -1,2 +1,9 @@
 #define buttonPin D1   
 #define pinLed D2
+
+unsigned long startTime;            // משתנה לשמירת זמן תחילת הלחיצה
+unsigned long endTime;              // משתנה לשמירת זמן סיום הלחיצה
+unsigned long totalTime;            //משתנה לשמירת הפרש הזמן מתחילת לחיצה לסיות לחיצה
+unsigned long lastPresses[10];      // מערך לשמירת 10 הלחיצות האחרונות
+int idx = 0;
+bool arrayIsFull = false;       // משתנה לשמירת מצב הביצועים
