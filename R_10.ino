39c643b46f99231d90ef403323d5db8127852bda
AvshalomSarusi <131193715+AvshalomSarusi@users.noreply.github.com>
2024-08-17 21:19:12 +0300

    Update R_10.ino
    יצירת פונקציית סאט אפ

--- R_10.ino
+++ R_10.ino
@@ -8,3 +8,13 @@ const char* pswd = "200492262";
 
 WiFiClient client;
 int server_port = 80;//http
+
+void wifiClient_Setup() {
+  Serial.println("wifiSetup");
+  WiFi.begin(ssid,pswd);
+    while (WiFi.status()!=WL_CONNECTED){
+      Serial.println("trying ...");
+      delay(100);
+    }
+      Serial.println("Connected to network");
+}
