850f115a9f06cd36b853a15d7e8cafd01c4c4a43
AvshalomSarusi <131193715+AvshalomSarusi@users.noreply.github.com>
2024-08-17 21:20:14 +0300

    Update R_10.ino
    יצירת פונקציית גאט דאטה

--- R_10.ino
+++ R_10.ino
@@ -18,3 +18,22 @@ void wifiClient_Setup() {
     }
       Serial.println("Connected to network");
 }
+
+int GetData() {
+  int ret = -1;
+  HTTPClient http;
+  String dataURL = "http://api.kits4.me/GEN/api.php?ACT=GET&DEV=1121&CH=1";
+  http.begin(client, dataURL);
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+  if (httpCode == HTTP_CODE_OK) {
+    Serial.print("HTTP response code ");
+    Serial.println(httpCode);
+    String Res = http.getString();
+    Serial.println(Res);
+    ret = Res.toInt();
+  }
+  http.end();
+
+  return ret;
+}
